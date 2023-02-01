65f94bc3ec0ed772266486fc130131338bddb107
Duong Pham <t0phtr00@students.oamk.fi>
2023-02-01 22:15:11 +0200

    Update
    Week 6 Added

---
+++ Week 6/app/src/main/java/com/example/week_6/MainActivity.kt
@@ -0,0 +1,119 @@
+package com.example.week_6
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.week_6.ui.theme.Week6Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Week6Theme {
+                ScaffoldApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun ScaffoldApp() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ) {
+        composable(route = "Home") {
+            MainScreen(navController)
+        }
+        composable(route = "Info") {
+            InfoScreen(navController)
+        }
+        composable(route = "Settings") {
+            SettingsScreen(navController)
+        }
+    }
+}
+
+@Composable
+fun MainTopBar(title: String, navController: NavController) {
+    var expanded by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = { Text(title) },
+        actions = {
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    Icons.Filled.MoreVert,
+                    contentDescription = null
+                )
+            }
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                DropdownMenuItem(onClick = { navController.navigate("info") }) {
+                    Text("Info")
+                }
+                DropdownMenuItem(onClick = { navController.navigate("settings") }) {
+                    Text("Settings")
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun ScreenTopBar(title: String, navController: NavController) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp() }) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar("Home", navController) },
+        content = { Text(text = "Content for Info Screen") }
+    )
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun InfoScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Info", navController) },
+        content = { Text(text = "Content for Info Screen") },
+    )
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Settings", navController) },
+        content = { Text(text = "Content for Settings Screen") },
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Week6Theme {
+        ScaffoldApp()
+    }
+}
